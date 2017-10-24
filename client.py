d4c519d0aebdbdbbe84baa8025c73cba30ffa119
Sebas95 <sgonzalezquesada1@gmail.com>
2017-10-23 22:38:56 -0600

    some prints

--- client.py
+++ client.py
@@ -1,17 +1,20 @@
 import socket
 import sys
 
-
-# Create a TCP/IP socket
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# Connect the socket to the port where the server is listening
-server_address = ('localhost', 10011)
-sock.connect(server_address)
-try:
-       
-                print "Socket establecido con HOST1"
+print "Socket establecido con HOST1"
+while True:
+        # Create a TCP/IP socket
+        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        # Connect the socket to the port where the server is listening
+        server_address = ('localhost', 10011)
+        sock.connect(server_address)
+        try:
+               
+                
                 print "Ingrese la ruta de la imagen"
                 ruta = raw_input("")
+                if (ruta == "salir"):
+                        break
                 file = open("/home/sebastian95/Documents/GitHub/ContainerJobs/forest.jpg", "rb")
                 # Send data
                 content = file.read(1024)
@@ -22,15 +25,14 @@ try:
                         else:
                                 print "Enviando imagen ........"
                                 break
-                             
+                                     
                 print "Imagen enviada"
-                sock.sendall("g")
+                
                 file.close()
-                #data = sock.recv(100)     
-                #print data
-        #except IOError as e:
-         #   if e.errno == errno.EPIPE:
-          #      print "error"
-finally:
-    print >>sys.stderr, 'closing socket'
-    sock.close()
+                        #data = sock.recv(100)     
+                        #print data
+                #except IOError as e:
+                 #   if e.errno == errno.EPIPE:
+                  #      print "error"
+        finally:
+            sock.close()
--- server.py
+++ server.py
@@ -7,34 +7,34 @@ import numpy
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 # Bind the socket to the port
 server_address = ('localhost', 10011)
-print >>sys.stderr, 'starting up on %s port %s' % server_address
+#print >>sys.stderr, 'starting up on %s port %s' % server_address
+print "iniciando socket con HOST2"
 sock.bind(server_address)
 
 # Listen for incoming connections
 sock.listen(1)
-# Recibe los datos en trozos y reetransmite
-bandera = False
-while True:
-    bandera = False
+serverRun = True
+while serverRun:
+    wait = True
     f = open("/home/sebastian95/Documents/GitHub/ContainerJobs/nuevo.jpg", "wb")
     # Wait for a connection
     connection, client_address = sock.accept()
-    print >>sys.stderr, 'waiting for a connection'
-    while bandera == False:  
+    #print >>sys.stderr, 'waiting for a connection'
+    while wait == True:  
         try:
-            print >>sys.stderr, 'connection from', client_address
+            #print >>sys.stderr, 'connection from', client_address
             while True:
                 data = connection.recv(1024)
-                
                 if data:
                     f.write(data)
                 else:
-                    print >>sys.stderr, 'Imagen recibida', client_address
+                    #print >>sys.stderr, 'Imagen recibida', client_address
+                    print 'Imagen recibida'
                     connection.send("listo")
                     f.close()
-                    
-                    print("termino la conexion")
-                    bandera = True;
+                    #enviar a las capertas-------
+                    print("Imagen procesada")
+                    wait = False;
                     break;
                     
         finally:
